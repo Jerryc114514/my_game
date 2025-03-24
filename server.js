5bbc877267f9b067c08a044f3c23d9a581f466ec
Jerryc114514 <haojun.chen@springdaleprep.org>
2025-03-24 13:22:47 -0400

    server.js

--- server.js
+++ server.js
@@ -1,72 +1,58 @@
-const express = require("express")
-const app = express()
-const http = require("http").createServer(app)
-const io = require("socket.io")(http)
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-// 存储所有玩家信息
-const players = {}
-// 存储所有气球信息
-const gameBalloons = []
+// Store players and balloons data
+const players = {};
+const gameBalloons = [];
 
-// 提供静态文件
-app.use(express.static("public")) // public 目录存放 index.html, script.js 等文件
+// Serve static files from the public folder
+app.use(express.static("public"));
 
-// Socket.io 连接处理
+// Socket.io connection handling
 io.on("connection", (socket) => {
-  console.log("A user connected: " + socket.id)
+  console.log("A user connected: " + socket.id);
 
-  // 新玩家加入
+  // Add new player
   players[socket.id] = {
     id: socket.id,
     score: 0,
-  }
+  };
 
-  // 广播新玩家加入
-  io.emit("playerJoined", players[socket.id])
+  // Broadcast new player joining
+  io.emit("playerJoined", players[socket.id]);
 
-  // 发送当前所有玩家信息给新玩家
-  socket.emit("currentPlayers", players)
+  // Send current players to the new player
+  socket.emit("currentPlayers", players);
 
-  // 发送当前所有气球信息给新玩家
-  socket.emit("currentBalloons", gameBalloons)
+  // Send current balloons to the new player
+  socket.emit("currentBalloons", gameBalloons);
 
-  // 玩家射击
+  // Handle player shooting
   socket.on("playerShot", (balloonId) => {
-    // 查找气球
-    const balloonIndex = gameBalloons.findIndex((b) => b.id === balloonId)
-
+    const balloonIndex = gameBalloons.findIndex((b) => b.id === balloonId);
     if (balloonIndex !== -1) {
-      // 移除气球
-      const balloon = gameBalloons.splice(balloonIndex, 1)[0]
-
-      // 更新玩家分数
-      players[socket.id].score++
-
-      // 广播气球被击中
+      const balloon = gameBalloons.splice(balloonIndex, 1)[0];
+      players[socket.id].score++;
       io.emit("balloonPopped", {
         balloonId: balloonId,
         playerId: socket.id,
         playerScore: players[socket.id].score,
-      })
-
-      // 创建新气球
-      createNewBalloon()
+      });
+      createNewBalloon();
     }
-  })
+  });
 
-  // 玩家断开连接
+  // Handle player disconnect
   socket.on("disconnect", () => {
-    console.log("User disconnected: " + socket.id)
+    console.log("User disconnected: " + socket.id);
+    delete players[socket.id];
+    io.emit("playerLeft", socket.id);
+  });
+});
 
-    // 移除玩家
-    delete players[socket.id]
-
-    // 广播玩家离开
-    io.emit("playerLeft", socket.id)
-  })
-})
-
-// 创建新气球
+// Create new balloon and broadcast it
 function createNewBalloon() {
   const newBalloon = {
     id: Date.now().toString(),
@@ -75,22 +61,20 @@ function createNewBalloon() {
       y: Math.random() * 5 - 2,
       z: -Math.random() * 10 - 5,
     },
-  }
+  };
 
-  gameBalloons.push(newBalloon)
-
-  // 广播新气球
-  io.emit("newBalloon", newBalloon)
+  gameBalloons.push(newBalloon);
+  io.emit("newBalloon", newBalloon);
 }
 
-// 初始化一些气球
+// Initialize some balloons
 for (let i = 0; i < 5; i++) {
-  createNewBalloon()
+  createNewBalloon();
 }
 
-// 启动服务器
-const PORT = process.env.PORT || 3000
+// Start server
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
 
